6943cfd69fb2de5cdaf34fd53820ccac69a6f974
Thomas R. Dial <dialtr@gmail.com>
2019-07-19 19:41:36 -0400

    add commentary

--- demo.c
+++ demo.c
@@ -6,15 +6,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+// In order to gain a better understanding of the order in which SQLite
+// calls module functions, we print a message to standard error at the
+// start of each function. The TRACE_FUNCTION() macro simplifies this.
 #define TRACE_FUNCTION() fprintf(stderr, "TRACE: %s\n", __FUNCTION__)
 
+// Our module will be named "demo".
 const char *DEMO_MODULE_NAME = "demo";
 
 // The schema for our demo table. For the demo, the table schema
 // consists of a single column named 'value', which is an integer.
 // The implementation of the virtual table informs the engine of
 // the schema by calling 'sqlite3_declare_vtab'. The name of the
-// table and any constraints are ignored.
+// table and any constraints are ignored. In this example, we
+// declare the schema in the DemoConnect function, below.
 const char *DEMO_TABLE_SCHEMA =
     "CREATE TABLE xx( "
     "  value INTEGER "
@@ -31,6 +36,12 @@ int DemoCreate(sqlite3 *db, void *pAux, int argc, const char *const *argv,
   return SQLITE_ERROR;
 }
 
+// Implementation of the "xConnect" function. Implementations of this function
+// are responsible for allocating a structure to track information about the
+// table; this initial fields of this structure must share the same layout as
+// the sqlite3_vtab structure, but may include additional fields as well. In
+// this example, we allocate an instance of DemoTable using SQLite's malloc
+// routine, and return it in ppVTab.
 int DemoConnect(sqlite3 *db, void *pAux, int argc, const char *const *argv,
                 sqlite3_vtab **ppVTab, char **pzErr) {
   TRACE_FUNCTION();
@@ -63,14 +74,26 @@ int DemoConnect(sqlite3 *db, void *pAux, int argc, const char *const *argv,
   return SQLITE_OK;
 }
 
+// SQLite calls the xBestIndex function during the planning phase in order
+// to determine the possible way to proceed with the query. The implementer
+// of this function has a chance to examine aspects of the query (provided
+// by the sqlite3_index_info* structure) and make recommendations of the
+// cost back to the engine. The implementation also returns an integer
+// identifier of the index number (also a field in sqlite3_index_info).
+// The engine may call this function more than once. Ultimately, when it
+// invokes the filter function to begin the "search" of the virtual table,
+// it passes back the index identifier that it wants to use based on its
+// cost determination.
 int DemoBestIndex(sqlite3_vtab *pVTab, sqlite3_index_info *info) {
   TRACE_FUNCTION();
 
-  // TODO(tdial): A real implementation would touch the structures.
+  // TODO(tdial): Implement
 
   return SQLITE_OK;
 }
 
+// Implementations of xDisconnect are responsible for cleaning up data
+// structures used during the session.
 int DemoDisconnect(sqlite3_vtab *pVTab) {
   TRACE_FUNCTION();
 
@@ -81,12 +104,23 @@ int DemoDisconnect(sqlite3_vtab *pVTab) {
   return SQLITE_OK;
 }
 
+// Implementations of xDestroy are responsible for actually destroying any
+// backing store. For example, if this implementation created files on disk,
+// xDestroy would be expected to purge them. In our example, we have nothing
+// to destroy, so we just return error. (Note: It may be more correct to
+// return SQLITE_OK.)
 int DemoDestroy(sqlite3_vtab *pVTab) {
   TRACE_FUNCTION();
   // TODO(tdial): Implement
   return SQLITE_ERROR;
 }
 
+// Routine the implements the opening of a cursor structure, which is really
+// just an iterator over a result set. The cursor structure returned by the
+// module must share the layout of sqlite3_vtab_cursor, but the struct
+// may include additional fields for module-specific information. In this
+// example, we allocate an instance of DemoCursor, and return a pointer to
+// it in ppCursor after casting it to the generic engine type.
 int DemoOpen(sqlite3_vtab *pVTab, sqlite3_vtab_cursor **ppCursor) {
   TRACE_FUNCTION();
   DemoCursor *cursor = (DemoCursor *)sqlite3_malloc64(sizeof(DemoCursor));
@@ -102,6 +136,7 @@ int DemoOpen(sqlite3_vtab *pVTab, sqlite3_vtab_cursor **ppCursor) {
   return SQLITE_OK;
 }
 
+// The xClose function implements cursor cleanup.
 int DemoClose(sqlite3_vtab_cursor *pCursor) {
   TRACE_FUNCTION();
   DemoCursor *cursor = (DemoCursor *)pCursor;
@@ -109,6 +144,11 @@ int DemoClose(sqlite3_vtab_cursor *pCursor) {
   return SQLITE_OK;
 }
 
+// The xFilter function is invoked by the engine during execution, and would
+// be the appropriate place to arrange for the iteration of of the data set,
+// make an external call to an API, etc.  The xFilter function is called with
+// index parameters chosen by the engine from those provided by prior calls to
+// the xBestIndex function.
 int DemoFilter(sqlite3_vtab_cursor *cursor, int idxNum, const char *idxStr,
                int argc, sqlite3_value **argv) {
   TRACE_FUNCTION();
@@ -116,6 +156,7 @@ int DemoFilter(sqlite3_vtab_cursor *cursor, int idxNum, const char *idxStr,
   return SQLITE_OK;
 }
 
+// Called to advance the cursor to the next position in the results.
 int DemoNext(sqlite3_vtab_cursor *pCursor) {
   TRACE_FUNCTION();
   DemoCursor *cursor = (DemoCursor *)pCursor;
@@ -123,12 +164,17 @@ int DemoNext(sqlite3_vtab_cursor *pCursor) {
   return SQLITE_OK;
 }
 
+// Invoked to determine whether the cursor has reached the end of results.
 int DemoEof(sqlite3_vtab_cursor *pCursor) {
   TRACE_FUNCTION();
   DemoCursor *cursor = (DemoCursor *)pCursor;
   return (cursor->n >= 10);
 }
 
+// Invoked by the engine to retrieve a column from the tuple that the cursor
+// currently points to. Results are reported to the engine via, the:
+// sqlite3_result_xxx family of functions. In this example, we're returning
+// an integer value.
 int DemoColumn(sqlite3_vtab_cursor *pCursor, sqlite3_context *ctx, int i) {
   TRACE_FUNCTION();
   DemoCursor *cursor = (DemoCursor *)pCursor;
@@ -140,6 +186,7 @@ int DemoColumn(sqlite3_vtab_cursor *pCursor, sqlite3_context *ctx, int i) {
   return SQLITE_OK;
 }
 
+// Return a unique rowid for the row.
 int DemoRowid(sqlite3_vtab_cursor *pCursor, sqlite_int64 *pRowid) {
   TRACE_FUNCTION();
   DemoCursor *cursor = (DemoCursor *)pCursor;
@@ -228,6 +275,9 @@ int DemoShadowName(const char *name) {
   return SQLITE_ERROR;
 }
 
+// Inform the engine of our module's functions. This example implements a
+// simple read-only table with one column, so we don't provide any of the
+// functions involved with writing to the database.
 int RegisterDemo(sqlite3 *db) {
   TRACE_FUNCTION();
   static sqlite3_module module = {
@@ -256,52 +306,6 @@ int RegisterDemo(sqlite3 *db) {
       0,               // xRollbackTo
       0,               // xShadowName
   };
-  return sqlite3_create_module(db, DEMO_MODULE_NAME, &module, 0);
-}
-
-/*
-// Register the demo table.
-int RegisterDemo(sqlite3 *db) {
-  TRACE_FUNCTION();
-
-  sqlite3_module module;
-  memset(&module, 0, sizeof(module));
-
-  module.iVersion = 0;
-
-  // xCreate and xConnect are both passed "DemoConnect"
-  // because this is an eponymous virtual table.
-  module.xCreate = DemoConnect;
-  module.xConnect = DemoConnect;
-  module.xBestIndex = DemoBestIndex;
-
-  // Disconnect and Destroy are both passed "DemoDisconnect" because
-  // our table has no backing store. We just want to free the table
-  // structure that happens.
-  module.xDisconnect = DemoDisconnect;
-  module.xDestroy = DemoDisconnect;
-
-  module.xOpen = DemoOpen;
-  module.xClose = DemoClose;
-  module.xFilter = DemoFilter;
-  module.xNext = DemoNext;
-  module.xEof = DemoEof;
-  module.xColumn = DemoColumn;
-  module.xRowid = DemoRowid;
-
-  // We don't support writing at this time in our demo.
-  module.xUpdate = NULL;        // DemoUpdate;
-  module.xBegin = NULL;         // DemoBegin;
-  module.xSync = NULL;          // DemoSync;
-  module.xCommit = NULL;        // DemoCommit;
-  module.xRollback = NULL;      // DemoRollback;
-  module.xFindFunction = NULL;  // DemoFindFunction;
-  module.xRename = NULL;        // DemoRename;
-  module.xSavepoint = NULL;     // DemoSavepoint;
-  module.xRelease = NULL;       // DemoRelease;
-  module.xRollbackTo = NULL;    // DemoRollbackTo;
-  module.xShadowName = NULL;    // DemoShadowName;
 
   return sqlite3_create_module(db, DEMO_MODULE_NAME, &module, 0);
 }
-*/
